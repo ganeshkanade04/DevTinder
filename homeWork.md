53b323e0bc9c627cea7b621916eae828f373b688
ganeshkanade04 <ganeshkanade04@gmail.com>
2025-07-30 15:37:59 +0530

    add skip and limit fuctionality(pagination)

--- homeWork.md
+++ homeWork.md
@@ -98,3 +98,12 @@ and next() function
 -thought process of POST VS GET API
 -Read about ref and populate 
 -create GET /user/requests/received
+
+
+
+//find only 10 user in pages
+/feed?page=1&limit=10 ==>1-10==>.skip(0).limit(10)
+/feed?page=2&limit=10 ==>11-20==>.skip(10).limit(10)
+/feed?page=3&limit=10 ==>21-30=>.skip(20).limit(10)
+
+skip=(page-1)*limit;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const userRouter=express.Router();
 const {userAuth}=require('../middlewares/auth');
+const User=require("../models/user")
 const ConnectionRequestModel=require('../models/connectionRequest');
+const USER_SAFE_DATA="firstName lastName age gender skills"
 userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
     try{
            const loggedInUser=req.user;
@@ -44,6 +46,55 @@ userRouter.get("/user/connection",userAuth,async(req,res)=>{
            })
     }
     catch(error){
+         res.send("ERROR :"+error.message);
+    }
+})
+
+userRouter.get("/feed",userAuth,async(req,res)=>{
+    try{
+        //User see all the user cards except
+        //0. his own card
+        //1. hi connection 
+        //2. ignored people
+        //3.already sent the connection request
+
+        //rahul is login rehul send request =[ganesh,omkar,samruddhi,mayur,ram];
+        //rahul send request to ganesh
+        //rahul=[omkar,samruddhi,mayur,ram]
+        //rahul rejected//accepted ram profile and do not show again to ram profile to rahul
+         
+        const loggedInUser=req.user;
+        const page=parseInt(req.query.page)||1;
+        let limit=parseInt(req.query.limit)||10;
+         limit=limit>50?50:limit;
+        const skip=(page-1)*limit;
+        //find all connection requect both send and received
+        const connectionRequest=await ConnectionRequestModel.find({
+            $or:[
+                {fromUserId:loggedInUser._id},
+                {toUserId:loggedInUser._id},
+            ]
+         }).select({ fromUserId: 1, toUserId: 1 })
+         //.populate("fromUserId","firstName")
+        // .populate("toUserId","firstName");;
+    //single object sathi("fromUserId","toUserId")
+
+    const hideUserFromFeed=new Set();
+    connectionRequest.forEach((req) => {
+        hideUserFromFeed.add(req.fromUserId.toString());
+        hideUserFromFeed.add(req.toUserId.toString());
+    });
+    
+
+    const remainingUser=await User.find({
+       $and:[{_id:{ $nin:Array.from(hideUserFromFeed)},},
+        {_id:{$ne:loggedInUser._id}}     
+    ]}).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+
+        res.json({remainingUser});
+    }
+     catch(error){
          res.send("ERROR :"+error.message);
     }
 })
