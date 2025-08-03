4e7e7430227031d0bac4bf5dbdf767a3e6569090
ganeshkanade04 <ganeshkanade04@gmail.com>
2025-08-03 14:27:57 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express=require("express");
 const app=express();
 const PORT=3000;
+const cors=require('cors');
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}));
 app.use(express.json());
 const cookieParser=require('cookie-parser');
 app.use(cookieParser());
